744d2e9dd74ffd0a7dad6098d5090f7537a1a799
svgooogle <51333628+svgooogle@users.noreply.github.com>
2025-11-29 01:57:56 +0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Create a virtual environment with Python 3.11.9
+python3.11 -m venv .venv
+source .venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
